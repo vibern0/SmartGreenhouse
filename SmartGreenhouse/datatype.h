071c6455ea693e1c35a1082c1ba6c14d8f06bf42
BernardoVieira <BernardoVieira>
2016-09-15 15:22:52 +0100

    InitialUpload
    First upload. Data changed, saved in EEPROM

---
+++ SmartGreenhouse/datatype.h
@@ -0,0 +1,11 @@
+#ifndef DATATYPE_HEADER 
+#define DATATYPE_HEADER
+
+struct component_
+{
+    bool stat;
+    byte turn_on;
+    byte turn_off;
+};
+    
+#endif
---
+++ SmartGreenhouse/memory.h
@@ -0,0 +1,29 @@
+#ifndef MEMORY_HEADER 
+#define MEMORY_HEADER
+
+#define MEMORY_VERSION            1
+
+#define MEMORY_ADRESS_STATUS      0
+#define MEMORY_ADRESS_COMPONENT   1
+
+void saveData(struct component_ component)
+{
+    EEPROM.put(MEMORY_ADRESS_COMPONENT, component);
+    if(EEPROM.read(MEMORY_ADRESS_STATUS) != MEMORY_VERSION)
+    {
+        EEPROM.write(MEMORY_ADRESS_STATUS, MEMORY_VERSION);
+    }
+    Serial.println("Data saved!");
+}
+struct component_ loadData()
+{
+    struct component_ component;
+    if(EEPROM.read(MEMORY_ADRESS_STATUS) == MEMORY_VERSION)
+    {
+        EEPROM.get(MEMORY_ADRESS_COMPONENT, component);
+        EEPROM.write(MEMORY_ADRESS_STATUS, MEMORY_VERSION);
+    }
+    return component;
+}
+    
+#endif
