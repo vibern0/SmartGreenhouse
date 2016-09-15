45295bc7d2dc5d46c50ef177bbd248d5683e8189
BernardoVieira <BernardoVieira>
2016-09-15 20:41:49 +0100

    BasicStructAndMemory
    - Basic structs for components - Memory save and load

--- SmartGreenhouse/SmartGreenhouse.ino
+++ SmartGreenhouse/SmartGreenhouse.ino
@@ -1,53 +1,21 @@
 #include <EEPROM.h>
 #include "datatype.h"
 #include "memory.h"
+#include "components.h"
+#include "input.h"
 
-
-struct component_ component_1;
-
-
+struct components_ components;
 
 void setup()
 {
     Serial.begin(9600);
-    component_1 = loadData();
+    components = loadData();
 }
 
 void loop()
 {
     if(Serial.available() > 0)
     {
-        char input[16];
-        byte len = Serial.available();
-        Serial.readBytes(input, 16);
-        input[len] = '\0';
-        
-        if(!strcmp(input, "c1 on"))
-        {
-            byte order = 1;
-            Serial.print("Time : ");
-            component_1.turn_on = 0;
-            while(Serial.available() == 0) { }
-            while(Serial.available() > 0)
-            {
-                component_1.turn_on = component_1.turn_on * order + byte(Serial.read()) - 48;
-                order *= 10;
-            }
-            Serial.println(component_1.turn_on);
-            Serial.println("Turn on time changed!");
-        }
-        else if(!strcmp(input, "print"))
-        {
-            Serial.print("Stat : ");
-            Serial.println(component_1.stat);
-            Serial.print("Turn on : ");
-            Serial.println(component_1.turn_on);
-            Serial.print("Turn off : ");
-            Serial.println(component_1.turn_off);
-        }
-        else if(!strcmp(input, "save"))
-        {
-            saveData(component_1);
-        }
+        components = checkInput(components);
     }
 }
--- SmartGreenhouse/memory.h
+++ SmartGreenhouse/memory.h
@@ -1,29 +1,39 @@
 #ifndef MEMORY_HEADER 
 #define MEMORY_HEADER
 
-#define MEMORY_VERSION            1
+#define MEMORY_VERSION            2
 
 #define MEMORY_ADRESS_STATUS      0
 #define MEMORY_ADRESS_COMPONENT   1
 
-void saveData(struct component_ component)
+void saveData(struct components_ components)
 {
-    EEPROM.put(MEMORY_ADRESS_COMPONENT, component);
+    EEPROM.put(MEMORY_ADRESS_COMPONENT, components);
     if(EEPROM.read(MEMORY_ADRESS_STATUS) != MEMORY_VERSION)
     {
         EEPROM.write(MEMORY_ADRESS_STATUS, MEMORY_VERSION);
     }
-    Serial.println("Data saved!");
+    Serial.println("Components data saved!");
 }
-struct component_ loadData()
+struct components_ loadData()
 {
-    struct component_ component;
+    struct components_ components;
     if(EEPROM.read(MEMORY_ADRESS_STATUS) == MEMORY_VERSION)
     {
-        EEPROM.get(MEMORY_ADRESS_COMPONENT, component);
+        EEPROM.get(MEMORY_ADRESS_COMPONENT, components);
         EEPROM.write(MEMORY_ADRESS_STATUS, MEMORY_VERSION);
+        Serial.println("Components data loaded!");
     }
-    return component;
+    else
+    {
+        for(byte i = 0; i < TOTAL_COMPONENTS; i++)
+        {
+            components.comp[i].status = false;
+            components.comp[i].turn_on = 0;
+            components.comp[i].turn_off = 0;
+        }
+    }
+    return components;
 }
     
 #endif
